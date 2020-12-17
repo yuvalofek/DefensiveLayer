f8fb68dac343c238b95e685e93284ab53c44134c
jkurian <jkurian49@gmail.com>
2020-12-16 22:55:47 -0500

    purged

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for model in vgg19
-do
-    echo "python main.py  --arch=$model  --save-dir=save_$model 2>&1 | tee -a log_$model"
-    python main_finetune.py  --arch=$model  --save-dir=save_$model 2>&1 | tee -a log_$model
-done
